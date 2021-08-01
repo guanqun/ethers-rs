14a6ae18a7c9a7ae387660e446ac5aade4c2e6b9
Georgios Konstantopoulos <me@gakonst.com>
2021-08-01 22:08:44 +0300

    feat: make trace_call / call take TypedTransaction

--- ethers-providers/src/lib.rs
+++ ethers-providers/src/lib.rs
@@ -336,7 +336,7 @@ pub trait Middleware: Sync + Send + Debug {
 
     async fn call(
         &self,
-        tx: &TransactionRequest,
+        tx: &TypedTransaction,
         block: Option<BlockId>,
     ) -> Result<Bytes, Self::Error> {
         self.inner().call(tx, block).await.map_err(FromErr::from)
@@ -501,9 +501,9 @@ pub trait Middleware: Sync + Send + Debug {
     // Parity `trace` support
 
     /// Executes the given call and returns a number of possible traces for it
-    async fn trace_call(
+    async fn trace_call<T: Into<TypedTransaction> + Send + Sync>(
         &self,
-        req: TransactionRequest,
+        req: T,
         trace_type: Vec<TraceType>,
         block: Option<BlockNumber>,
     ) -> Result<BlockTrace, Self::Error> {

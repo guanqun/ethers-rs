502d8ac86a703ff812b7356a13937bee9cc589da
Guanqun Lu <guanqunlu@outlook.com>
2022-01-09 09:41:28 +0800

    Add api to get multiple storages

--- ethers-providers/src/provider.rs
+++ ethers-providers/src/provider.rs
@@ -32,6 +32,8 @@ use std::{convert::TryFrom, fmt::Debug, str::FromStr, sync::Arc, time::Duration}
 use tracing::trace;
 use tracing_futures::Instrument;
 
+use itertools::Itertools;
+
 #[derive(Copy, Clone)]
 pub enum NodeClient {
     Geth,
@@ -640,6 +642,23 @@ impl<P: JsonRpcClient> Middleware for Provider<P> {
         // get rid of the 0x prefix and left pad it with zeroes.
         let value = format!("{:0>64}", value.replace("0x", ""));
         Ok(H256::from_slice(&Vec::from_hex(value)?))
+    }
+
+    async fn get_multiple_storages(&self, froms: Vec<Address>, locations: Vec<H256>, block: Option<BlockId>) -> Result<Vec<Vec<H256>>, ProviderError> {
+        let froms = utils::serialize(&froms);
+        let locations = utils::serialize(&locations);
+        let block = utils::serialize(&block.unwrap_or_else(|| BlockNumber::Latest.into()));
+
+        // get the hex encoded value.
+        let values: Vec<Vec<String>> = self.request("eth_getMultipleStorages", [froms, locations, block]).await?;
+
+        let ret: Vec<Vec<H256>> = values.into_iter().map(|inner_vec| inner_vec.into_iter().map(|v| {
+            // get rid of the 0x prefix and left pad it with zeroes.
+            let value = format!("{:0>64}", v.replace("0x", ""));
+            Vec::from_hex(value).map(|hex_vec| H256::from_slice(&hex_vec))
+        }).try_collect()).try_collect()?;
+
+        Ok(ret)
     }
 
     /// Returns the deployed code at a given address
